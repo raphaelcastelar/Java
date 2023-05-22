925ccbf49d4b0c4e7786110be047595ed45445cd
raphaelcastelar <castelaraphael@gmail.com>
2023-05-22 18:17:34 -0300

    java atualização

---
+++ exerciciosjava/ex02.java
@@ -0,0 +1,12 @@
+package exerciciosjava;
+
+public class ex02 {
+    public static void main(String[] args) throws Exception {
+        
+        
+    System.out.println("Hello, World!");
+
+    }
+
+
+}
