3be5d3d7292f1ce3d2256e0027c052a3599fcedc
raphaelcastelar <castelaraphael@gmail.com>
2023-05-22 19:38:35 -0300

    hora do sistema

---
+++ horadosistema/src/Horadosistema.java
@@ -0,0 +1,9 @@
+import java.util.Date;
+
+public class Horadosistema {
+    public static void main(String[] args) throws Exception {
+    Date relogio = new Date(); 
+    System.out.println("a hora do sistema  e");
+    System.out.println(relogio.toString());  
+    }
+}
