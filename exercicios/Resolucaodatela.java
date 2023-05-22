04b3c089d9849b2fa88d98e22cdb2f8402e4625f
raphaelcastelar <castelaraphael@gmail.com>
2023-05-22 19:49:49 -0300

    resolução

---
+++ exercicios/Resolucaodatela.java
@@ -0,0 +1,11 @@
+
+import java.lang.module.ResolutionException;
+
+package exercicios{
+
+public class Resolucaodatela {
+    public static void main(String[] args) {
+        System.out.println("ola mundo");
+        
+    }
+}
---
+++ resolucaodetela/src/App.java
@@ -0,0 +1,14 @@
+
+import java.awt.Dimension;
+import java.awt.Toolkit;
+
+
+public class App {
+    public static void main(String[] args) {
+        Toolkit tk;
+        tk = Toolkit.getDefaultToolkit();
+        Dimension d = tk.getScreenSize();
+        System.out.print("Sua resolucao e de "+d.width);
+        System.out.println(" X "+d.height);
+    }
+}
