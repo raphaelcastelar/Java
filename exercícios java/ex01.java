9721ac72cd54528c6ddef765c4b818c4d8a5d41c
raphaelcastelar <castelaraphael@gmail.com>
2023-05-22 18:12:56 -0300

    primeiro exercício java

---
+++ exercícios java/ex01.java
@@ -0,0 +1,13 @@
+public class ex01 {
+    public static void main(String[] args) throws Exception {
+        int numero = -10;
+        String resultado = "";
+        if (numero  >= 0){
+            resultado = "positivo";
+        }else{
+         resultado = "negativo";    
+
+        }
+        System.out.println("Hello, World! O numero e:"+resultado);
+    }
+}
