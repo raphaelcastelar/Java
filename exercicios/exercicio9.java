9d01f6fbd5da57c6b504e7a8dcb415f50f35f45c
raphaelcastelar <castelaraphael@gmail.com>
2023-06-22 15:33:52 -0300

    java att exercicios

--- exercicios/exercicio9.java
+++ exercicios/exercicio9.java
@@ -3,6 +3,14 @@ import java.util.Scanner;
 public class exercicio9 {
     public static void main(String[] args) {
         Scanner leitorTeclado = new Scanner(System.in);
-        
+        double salario, reajuste, newsalario;
+        System.out.print("Informe o salário atual: ");
+        salario = leitorTeclado.nextDouble();
+        System.out.print("Qual a porcentagem do reajuste: ");
+        reajuste = leitorTeclado.nextDouble();
+        newsalario = salario + (salario * (reajuste / 100));
+        System.out.println("O novo salário é de: " + newsalario);
+
+
     }
 }
