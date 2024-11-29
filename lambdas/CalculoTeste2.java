0380df61c20c4dc2b3f32c5fa9d578f1bb33317f
Raphael Castelar <130313902+raphaelcastelar@users.noreply.github.com>
2024-11-29 13:35:07 -0300

    lambda 2

--- lambdas/CalculoTeste2.java
+++ lambdas/CalculoTeste2.java
@@ -3,11 +3,12 @@ package lambdas;
 public class CalculoTeste2{
     public static void main(String[] args) {
         
-        Calculo calculo = (x,y) -> {
-            return x + y;
-        };
+        Calculo calc = (x,y) -> {return x + y;};
 
-        System.out.println(calculo.executar(2,3));
+        System.out.println(calc.executar(2,3));
+        
+        calc  = (x,y) -> x * y;
+        System.out.println(calc.executar(2, 3));
     }
 
 }
