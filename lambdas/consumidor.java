9672f16c4ec7c453e810b6002eeec028d98596d9
raphael castelar <130313902+raphaelcastelar@users.noreply.github.com>
2024-12-11 17:08:32 -0300

    lambda 12

--- lambdas/consumidor.java
+++ lambdas/consumidor.java
@@ -5,6 +5,9 @@ import java.util.function.Consumer;
 public class consumidor {
     public static void main(String[] args) {
         Consumer<Produto> imprimir = p ->System.out.println(p.nome);
+        
+        Produto p1 = new Produto("Caneta", 12.34, 0.09);
+        imprimir.accept(p1);
     }
     
 }
